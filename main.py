fac44431ec8471275cb2b033fa8e508de7718b1d
steven <ssss91809@gmail.com>
2024-03-07 21:43:21 +0800

    完成圖例

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
 from datetime import datetime
 from pm25 import get_pm25
-
+import json
 
 books = {1: "Python book", 2: "Java book", 3: "Flask book"}
 
 app = Flask(__name__)
+
+
+@app.route("/pm25-charts")
+def pm25_charts():
+    return render_template("pm25-charts.html")
+
+
+@app.route("/pm25-data", methods=["GET"])
+def pm25_data():
+    columns, values = get_pm25()
+
+    site = [value[0] for value in values]
+    pm25 = [value[2] for value in values]
+    datetime = values[0][-2]
+
+    result = json.dumps(
+        {"datetime": datetime, "site": site, "pm25": pm25}, ensure_ascii=False
+    )
+
+    return result
+    print(site, pm25)
 
 
 @app.route("/pm25")
@@ -71,4 +92,5 @@ def index():
     return render_template("index.html", time=now, name="Jerry")
 
 
+print(pm25_data())
 app.run(debug=True)
